85a5c4e9e4f4437ee06edaeb87f977cdd249e9a0
BV Satyaram <bvsatyaram@gmail.com>
2016-08-30 21:56:46 +0530

    Seed Fake Meals

--- CalTrackBackend/db/seeds.rb
+++ CalTrackBackend/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def dot
+  print '.'
+end
+
+puts 'Destroying Existing Meals'
+Meal.destroy_all
+
+puts 'Seeding New Meals'
+(0...10).each do |i|
+  (5 + rand(5)).times do
+    meal = Meal.new
+    meal.title = Faker::Beer.name
+    meal.time = Date.today - i.days + rand(86400).seconds
+    meal.calories = (1 + rand(15))*100
+    meal.save
+    dot
+  end
+end
+
+puts ''
+puts 'Done'
