957a2ed93a6a38043aa34ae015dd8a425434e97c
BV Satyaram <bvsatyaram@gmail.com>
2016-08-30 23:40:00 +0530

    Authentication on API

--- CalTrackBackend/config/routes.rb
+++ CalTrackBackend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   api_version(:module => "V1", :path => {:value => "v1"}) do
+    mount_devise_token_auth_for 'User', at: 'auth'
     resources :meals
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
