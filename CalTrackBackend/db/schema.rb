bf303f7543aa10bac64b56abaaa3175b57f6e240
BV Satyaram <bvsatyaram@gmail.com>
2016-08-31 01:01:34 +0530

    Add admin role to users

--- CalTrackBackend/db/schema.rb
+++ CalTrackBackend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830180137) do
+ActiveRecord::Schema.define(version: 20160830192422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160830180137) do
     t.json     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+    t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
