c8a99c9d5d039fb671abb139788648e943cea0d7
BV Satyaram <bvsatyaram@gmail.com>
2016-08-30 21:28:27 +0530

    Add CORS to API

--- CalTrackBackend/config/environments/development.rb
+++ CalTrackBackend/config/environments/development.rb
@@ -51,4 +51,36 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Use Rack-Cors for enabling CORS on development
+  # Might have to be removed on production deployment
+  config.middleware.use Rack::Cors do
+    allow do
+      origins 'localhost:3000', '127.0.0.1:3000',
+      /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+      # regular expressions can be used here
+
+      resource '/file/list_all/', :headers => 'x-domain-token'
+      resource '/file/at/*',
+      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+      :headers => 'x-domain-token',
+      :expose  => ['Some-Custom-Response-Header'],
+      :max_age => 600
+      # headers to expose
+    end
+
+    allow do
+      origins '*'
+      resource '/public/*', :headers => :any, :methods => :get
+    end
+  end
+
+  # Rails 5 CORS middleware
+  # Might have to be removed on production deployment
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', :headers => :any, :methods => [:get, :post, :options, :delete], expose: ['access-token', 'client', 'token-type', 'uid', 'expiry']
+    end
+  end
 end
