573a33a7f932d5d398a27ead89bb4cebdece5b06
BV Satyaram <bvsatyaram@gmail.com>
2016-08-31 15:07:43 +0530

    Add charts using Chartist.js

--- CalTrackFrontend/src/client/app/tracker/chart/chart.component.ts
+++ CalTrackFrontend/src/client/app/tracker/chart/chart.component.ts
@@ -4,6 +4,8 @@ import {MealsService} from "../services/meals.service";
 import { Component, OnInit } from '@angular/core';
 import { Meal } from '../models/meal';
 import { NKDatetime } from 'ng2-datetime/ng2-datetime';
+declare var Chartist: any;
+
 /**
  * This class represents the lazy loaded AboutComponent.
  */
@@ -21,11 +23,12 @@ export class TrackerChartComponent implements OnInit {
   constructor(private currentUserService: CurrentUserService, private mealsSerivce: MealsService) {}
 
   ngOnInit() {
+    this.caloriesTarget = this.currentUserService.getCurrentUser().target_calories;
     this.mealsSerivce.mealsEmitter.subscribe((meals: Meal[]) => {
-      let currentMoment = moment();
+      let currentMoment = moment().add(1, 'days');
       let chartData = {};
       for (let i = 0; i < 10; i++) {
-        chartData[currentMoment.subtract(i, 'days').format('YYYY-MM-DD')] = 0;
+        chartData[currentMoment.subtract(1, 'days').format('YYYY-MM-DD')] = 0;
       }
       for (let meal of meals) {
         if (chartData[meal.dateString] !== undefined) {
@@ -34,7 +37,32 @@ export class TrackerChartComponent implements OnInit {
       }
 
       this.chartData = chartData;
-    })
-    this.caloriesTarget = this.currentUserService.getCurrentUser().target_calories;
+      this.drawChart()
+    });
+
   }
+
+  drawChart() {
+    let keys = Object.keys(this.chartData).reverse();
+    let values: any[] = [];
+    let targetValues: any[] = []
+    for (let key of keys) {
+      values.push(this.chartData[key]);
+      targetValues.push(this.caloriesTarget);
+    }
+
+    new Chartist.Line('#cal-chart', {
+      labels: keys,
+      series: [
+        values,
+        targetValues
+      ]
+    }, {
+      fullWidth: true,
+      chartPadding: {
+        right: 40
+      },
+      low: 0
+    });
+  };
 }
