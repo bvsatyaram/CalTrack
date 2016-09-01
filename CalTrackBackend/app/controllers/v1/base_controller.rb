0ae8ba0d350ea2ca3aab252f6d553e0824eca968
BV Satyaram <bvsatyaram@gmail.com>
2016-09-01 12:38:45 +0530

    Authorization for User

--- CalTrackBackend/app/controllers/v1/base_controller.rb
+++ CalTrackBackend/app/controllers/v1/base_controller.rb
@@ -1,3 +1,18 @@
 class V1::BaseController < ApplicationController
+  include CanCan::ControllerAdditions
+
   before_action :authenticate_v1_user!, unless: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    deny_access!
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_v1_user)
+  end
+
+protected
+  def deny_access!(msg = 'Access Denied')
+    render nothing: true, status: :unauthorized
+  end
 end
--- CalTrackBackend/app/controllers/v1/users_controller.rb
+++ CalTrackBackend/app/controllers/v1/users_controller.rb
@@ -2,10 +2,13 @@ class V1::UsersController < V1::BaseController
   before_action :fetch_user, only: [:update, :destroy]
 
   def index
+    deny_access! unless can? :index, User
     @users = User.all
   end
 
   def update
+    deny_access! unless can? :update, @user
+    deny_access! unless (params[:data][:attributes][:admin].nil? && params[:data][:attributes][:manager].nil?) || can?(:manage_role, @user)
     @user.target_calories = params[:data][:attributes][:target_calories]
     @user.admin = params[:data][:attributes][:admin] unless params[:data][:attributes][:admin].nil?
     @user.manager = params[:data][:attributes][:manager] unless params[:data][:attributes][:manager].nil?
@@ -17,6 +20,7 @@ class V1::UsersController < V1::BaseController
   end
 
   def destroy
+    deny_access! unless can? :destroy, @user
     @user.destroy
     render status: :no_content, nothing: :true
   end
