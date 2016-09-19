b1cd0305df2bd4592ae265e1774cacc22c29c736
BV Satyaram <bvsatyaram@gmail.com>
2016-09-19 22:28:10 +0530

    Add dependent destroy for users

--- CalTrackBackend/app/models/user.rb
+++ CalTrackBackend/app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
           #:confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :meals
+  has_many :meals, dependent: :destroy
 
   validates :target_calories, presence: true, numericality: true
 end
